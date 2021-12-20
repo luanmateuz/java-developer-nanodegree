81f6b182f581fc5e386c67aa8e6724696c14612c
luanmateuz <luanmateuzz@gmail.com>
2021-12-20 01:54:12 -0300

    lesson 02: using rest controller

--- part02/module01/spring-boot-helloworld/src/main/java/com/example/helloworld/HelloController.java
+++ part02/module01/spring-boot-helloworld/src/main/java/com/example/helloworld/HelloController.java
@@ -1,19 +1,17 @@
 package com.example.helloworld;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class HelloController {
 
     private Map<String, Object> result = new HashMap<>();
 
     @RequestMapping("/hello")
-    @ResponseBody
     public Map<String, Object> hello() {
         result.put("name", "Luan Mateus");
         result.put("city", "Federal District");
