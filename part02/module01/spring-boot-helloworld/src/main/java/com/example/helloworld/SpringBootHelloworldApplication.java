593db18a736f47ff860698c9e458846cc698a4ec
luanmateuz <luanmateuzz@gmail.com>
2021-12-20 02:15:34 -0300

    lesson 02: hello bean

--- part02/module01/spring-boot-helloworld/src/main/java/com/example/helloworld/SpringBootHelloworldApplication.java
+++ part02/module01/spring-boot-helloworld/src/main/java/com/example/helloworld/SpringBootHelloworldApplication.java
@@ -1,15 +1,43 @@
 package com.example.helloworld;
 
+import com.example.helloworld.servlet.HelloFilter;
+import com.example.helloworld.servlet.HelloListener;
+import com.example.helloworld.servlet.HelloServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@ServletComponentScan
 public class SpringBootHelloworldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloworldApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloworldApplication.class, args);
+    }
 
+    @Bean
+    public ServletRegistrationBean getServletRegistrationBean() {
+        ServletRegistrationBean servletBean = new ServletRegistrationBean(new HelloServlet());
+        servletBean.addUrlMappings("/helloServlet");
+
+        return servletBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean getFilterRegistrationBean() {
+        FilterRegistrationBean filterBean = new FilterRegistrationBean(new HelloFilter());
+        filterBean.addUrlPatterns("/helloServlet");
+
+        return filterBean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<HelloListener> getServletListenerRegistrationBean() {
+        ServletListenerRegistrationBean listenerBean =
+                new ServletListenerRegistrationBean(new HelloListener());
+
+        return listenerBean;
+    }
 }
