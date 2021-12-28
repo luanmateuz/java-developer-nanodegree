cca97468d65833d10bd7eecdb397546e11cb08c2
luanmateuz <luanmateuzz@gmail.com>
2021-12-28 14:50:43 -0300

    lesson 04: simpleMappingExceptionResolver

---
+++ part02/module01/spring-boot-exception/src/main/java/com/example/springbootexception/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.springbootexception.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/add")
+    public String add() {
+        int num = 10 / 0;
+
+        return "add";
+    }
+
+    @RequestMapping("/update")
+    public String update() {
+        String name = null;
+        name = name.toLowerCase();
+
+        return "update";
+    }
+
+}
