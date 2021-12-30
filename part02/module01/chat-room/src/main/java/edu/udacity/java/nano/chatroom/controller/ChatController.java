c3a31a92405adf8a6adc1962fed5c78c82268f5f
luanmateuz <luanmateuzz@gmail.com>
2021-12-30 19:51:31 -0300

    feat: login page
    Simple login page

---
+++ part02/module01/chat-room/src/main/java/edu/udacity/java/nano/chatroom/controller/ChatController.java
@@ -0,0 +1,14 @@
+package edu.udacity.java.nano.chatroom.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController("/")
+public class ChatController {
+
+    @GetMapping("")
+    public ModelAndView login() {
+        return new ModelAndView("login");
+    }
+}
