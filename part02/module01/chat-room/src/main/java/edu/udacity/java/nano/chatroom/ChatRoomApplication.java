dd14023ec177699a92a2f4eac50e8f40f73e962b
luanmateuz <luanmateuzz@gmail.com>
2021-12-30 19:19:17 -0300

    feat: start project
    Start Spring Boot with WebSocket, the goal of project is to do a simple web chat with websocket and add others dependencies if necessary

---
+++ part02/module01/chat-room/src/main/java/edu/udacity/java/nano/chatroom/ChatRoomApplication.java
@@ -0,0 +1,13 @@
+package edu.udacity.java.nano.chatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomApplication.class, args);
+	}
+
+}
