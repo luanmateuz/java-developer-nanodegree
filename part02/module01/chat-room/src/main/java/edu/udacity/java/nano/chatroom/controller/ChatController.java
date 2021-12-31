02702c6731e721a17940aa0b0f6d3e7dcb12b3fd
luanmateuz <luanmateuzz@gmail.com>
2021-12-30 22:32:45 -0300

    feat: chat page
    Simple static page for chat

--- part02/module01/chat-room/src/main/java/edu/udacity/java/nano/chatroom/controller/ChatController.java
+++ part02/module01/chat-room/src/main/java/edu/udacity/java/nano/chatroom/controller/ChatController.java
@@ -4,11 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController("/")
 public class ChatController {
 
     @GetMapping("")
     public ModelAndView login() {
         return new ModelAndView("login");
+    }
+
+    @GetMapping("/index")
+    public ModelAndView index(String username, HttpServletRequest request) {
+        ModelAndView mv = new ModelAndView("chat");
+        mv.addObject("username", username);
+
+        return mv;
     }
 }
