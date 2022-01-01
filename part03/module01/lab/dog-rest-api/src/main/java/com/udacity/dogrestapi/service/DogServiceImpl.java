257790392656ca46602cc57316b0bed439476b2b
luanmateuz <luanmateuzz@gmail.com>
2022-01-01 00:15:10 -0300

    feat: custom exception for error 404

--- part03/module01/lab/dog-rest-api/src/main/java/com/udacity/dogrestapi/service/DogServiceImpl.java
+++ part03/module01/lab/dog-rest-api/src/main/java/com/udacity/dogrestapi/service/DogServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DogServiceImpl implements DogService {
@@ -26,7 +27,8 @@ public class DogServiceImpl implements DogService {
 
     @Override
     public String retriveDogBreedById(Long id) {
-        return dogRepository.findBreedById(id);
+        Optional<String> optional = Optional.ofNullable(dogRepository.findBreedById(id));
+        return optional.orElseThrow(DogNotFoundException::new);
     }
 
     @Override
