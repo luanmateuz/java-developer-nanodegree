05dd08ba0dd69215c8fb44b711eefe6ecb987c2b
luanmateuz <luanmateuzz@gmail.com>
2022-01-13 20:06:31 -0300

    lab i: set up of dog graphql api

---
+++ part03/module01/lab/dog-graphql-api/src/test/java/com/udacity/doggraphqlapi/DogGraphqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.doggraphqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogGraphqlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
