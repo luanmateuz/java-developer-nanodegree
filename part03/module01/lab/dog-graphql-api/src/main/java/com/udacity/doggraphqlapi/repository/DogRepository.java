fb7696d54546d2be307e54ad278c866f475df847
luanmateuz <luanmateuzz@gmail.com>
2022-01-13 22:24:32 -0300

    lab ii: dog entity, repository and schema

---
+++ part03/module01/lab/dog-graphql-api/src/main/java/com/udacity/doggraphqlapi/repository/DogRepository.java
@@ -0,0 +1,7 @@
+package com.udacity.doggraphqlapi.repository;
+
+import com.udacity.doggraphqlapi.entity.Dog;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DogRepository extends CrudRepository<Dog, Long> {
+}
---
+++ part03/module01/lab/dog-graphql-api/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Dog (id, name, breed, origin) VALUES ( null, 'Sheik', 'Doguinho', 'American');
+INSERT INTO Dog (id, name, breed, origin) VALUES ( null, 'Luuq', 'Auggie', 'Astria');
+INSERT INTO Dog (id, name, breed, origin) VALUES ( null, 'Sweet', 'Basset', 'American');
+INSERT INTO Dog (id, name, breed, origin) VALUES ( null, 'Maik', 'Doberman Pinscher', 'American');
+INSERT INTO Dog (id, name, breed, origin) VALUES ( null, 'Kiria', 'Puggle', 'American');
+INSERT INTO Dog (id, name, breed, origin) VALUES ( null, 'Quora', 'Rottweiler', 'American');
