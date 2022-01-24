40293d8c25faa1e1c8753bb5a60c06059b06cd25
luanmateuz <luanmateuzz@gmail.com>
2022-01-23 23:10:23 -0300

    project: fix remove list to only price

--- part03/module01/project/pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
+++ part03/module01/project/pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
@@ -1,7 +1,6 @@
 package com.udacity.pricing.repository;
 
 import com.udacity.pricing.entity.Price;
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -11,5 +10,5 @@ import org.springframework.stereotype.Repository;
 @RepositoryRestResource(path = "price")
 public interface PriceRepository extends CrudRepository<Price, Long> {
 
-    List<Price> findByVehicleId(@Param(value = "id") Long id);
+    Price findByVehicleId(@Param(value = "id") Long id);
 }
