5331148b664c8804d15230125d90b41daa920eca
luanmateuz <luanmateuzz@gmail.com>
2022-01-23 22:16:31 -0300

    project: insert data on table price

---
+++ part03/module01/project/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO
+  Price
+  (id, currency, price, vehicle_id)
+VALUES
+  (null, 'USD', 20000, 1),
+  (null, 'USD', 15000, 2),
+  (null, 'USD', 20000, 3),
+  (null, 'USD', 52500, 4),
+  (null, 'USD', 11100, 5),
+  (null, 'USD', 30500, 6),
+  (null, 'USD', 15300, 7),
+  (null, 'USD', 22010, 8),
+  (null, 'USD', 95000, 9),
+  (null, 'USD', 10000, 10),
+  (null, 'USD', 11500, 11);
