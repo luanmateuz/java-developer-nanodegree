557fcd7be2ec6b3c2c8ca91f391797b35bc42b1b
luanmateuz <luanmateuzz@gmail.com>
2022-01-23 21:41:28 -0300

    project: beginning

---
+++ part03/module01/project/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTest.java
@@ -0,0 +1,14 @@
+package com.udacity.pricing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
