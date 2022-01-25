72229b3d40ddf653cbc4607c6eef5b5c7368d3e9
luanmateuz <luanmateuzz@gmail.com>
2022-01-25 10:12:48 -0300

    project: refactor tests

--- part03/module01/project/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ part03/module01/project/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -15,14 +15,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.udacity.vehicles.client.maps.MapsClient;
 import com.udacity.vehicles.client.prices.PriceClient;
 import com.udacity.vehicles.domain.Condition;
-import com.udacity.vehicles.domain.Location;
 import com.udacity.vehicles.domain.car.Car;
-import com.udacity.vehicles.domain.car.Details;
-import com.udacity.vehicles.domain.manufacturer.Manufacturer;
 import com.udacity.vehicles.service.CarNotFoundException;
 import com.udacity.vehicles.service.CarService;
+import com.udacity.vehicles.util.CarTest;
 import java.util.Collections;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
@@ -30,18 +29,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.web.servlet.MockMvc;
 
-/**
- * Implements testing of the CarController class.
- */
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
-public class CarControllerTest {
+class CarControllerTest {
 
     @Autowired
     private MockMvc mvc;
@@ -58,27 +52,19 @@ public class CarControllerTest {
     @MockBean
     private MapsClient mapsClient;
 
-    /**
-     * Creates pre-requisites for testing, such as an example car.
-     */
-
     @BeforeEach
     public void setup() {
-        Car car = getCar();
+        Car car = CarTest.createCar();
         car.setId(1L);
         given(carService.save(any())).willReturn(car);
         given(carService.findById(any())).willReturn(car);
         given(carService.list()).willReturn(Collections.singletonList(car));
     }
 
-    /**
-     * Tests for successful creation of new car in the system
-     *
-     * @throws Exception when car creation fails in the system
-     */
     @Test
-    public void createCar() throws Exception {
-        Car car = getCar();
+    @DisplayName("createCar returns a car when successful")
+    void createCar() throws Exception {
+        Car car = CarTest.createCar();
         mvc.perform(
                 post("/cars")
                         .content(json.write(car).getJson())
@@ -88,18 +74,9 @@ public class CarControllerTest {
                 .andExpect(status().isCreated());
     }
 
-    /**
-     * Tests if the read operation appropriately returns a list of vehicles.
-     *
-     * @throws Exception if the read operation of the vehicle list fails
-     */
     @Test
-    public void listCars() throws Exception {
-        /**
-         * TODO: Add a test to check that the `get` method works by calling
-         *   the whole list of vehicles. This should utilize the car from `getCar()`
-         *   below (the vehicle will be the first in the list).
-         */
+    @DisplayName("listCars returns a list of cars when successful")
+    void listCars() throws Exception {
         mvc.perform(
                 get("/cars"))
                 .andDo(print())
@@ -107,17 +84,9 @@ public class CarControllerTest {
                 .andExpect(jsonPath("$._embedded.carList[*].id").isNotEmpty());
     }
 
-    /**
-     * Tests the read operation for a single car by ID.
-     *
-     * @throws Exception if the read operation for a single car fails
-     */
     @Test
-    public void findCar() throws Exception {
-        /**
-         * TODO: Add a test to check that the `get` method works by calling
-         *   a vehicle by ID. This should utilize the car from `getCar()` below.
-         */
+    @DisplayName("findCar returns a car when successful")
+    void findCar() throws Exception {
         mvc.perform(
                 get("/cars/1"))
                 .andDo(print())
@@ -127,7 +96,8 @@ public class CarControllerTest {
     }
 
     @Test
-    public void findCarNotFoundException() throws Exception {
+    @DisplayName("findCarNotFoundException returns 404 when car is not found")
+    void findCarNotFoundException() throws Exception {
         given(carService.findById(any())).willThrow(CarNotFoundException.class);
 
         mvc.perform(
@@ -138,8 +108,9 @@ public class CarControllerTest {
     }
 
     @Test
-    public void updateCar() throws Exception {
-        Car car = getCar();
+    @DisplayName("updateCar updates car when successful")
+    void updateCar() throws Exception {
+        Car car = CarTest.createCar();
         car.setCondition(Condition.NEW);
 
         mvc.perform(
@@ -151,46 +122,13 @@ public class CarControllerTest {
                 .andExpect(status().isNoContent());
     }
 
-    /**
-     * Tests the deletion of a single car by ID.
-     *
-     * @throws Exception if the delete operation of a vehicle fails
-     */
     @Test
-    public void deleteCar() throws Exception {
-        /**
-         * TODO: Add a test to check whether a vehicle is appropriately deleted
-         *   when the `delete` method is called from the Car Controller. This
-         *   should utilize the car from `getCar()` below.
-         */
+    @DisplayName("deleteCar removes car when successful")
+    void deleteCar() throws Exception {
         mvc.perform(
                 delete("/cars/1"))
                 .andDo(print())
                 .andExpect(status().isNoContent());
     }
 
-    /**
-     * Creates an example Car object for use in testing.
-     *
-     * @return an example Car object
-     */
-    private Car getCar() {
-        Car car = new Car();
-        car.setLocation(new Location(40.730610, -73.935242));
-        Details details = new Details();
-        Manufacturer manufacturer = new Manufacturer(101, "Chevrolet");
-        details.setManufacturer(manufacturer);
-        details.setModel("Impala");
-        details.setMileage(32280);
-        details.setExternalColor("white");
-        details.setBody("sedan");
-        details.setEngine("3.6L V6");
-        details.setFuelType("Gasoline");
-        details.setModelYear(2018);
-        details.setProductionYear(2018);
-        details.setNumberOfDoors(4);
-        car.setDetails(details);
-        car.setCondition(Condition.USED);
-        return car;
-    }
 }
