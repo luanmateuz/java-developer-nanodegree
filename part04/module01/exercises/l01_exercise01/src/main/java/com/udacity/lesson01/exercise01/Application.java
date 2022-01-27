6a00f15a6b2fc7cab486525e71952bc2fbcabbe3
luanmateuz <luanmateuzz@gmail.com>
2022-01-27 00:14:13 -0300

    exercise 01: insert and read data from database

--- part04/module01/exercises/l01_exercise01/src/main/java/com/udacity/lesson01/exercise01/Application.java
+++ part04/module01/exercises/l01_exercise01/src/main/java/com/udacity/lesson01/exercise01/Application.java
@@ -2,7 +2,9 @@ package com.udacity.lesson01.exercise01;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import org.flywaydb.core.Flyway;
 import org.tinylog.Logger;
 
@@ -28,15 +30,31 @@ public class Application {
         // STEP 3: Obtain a connection to the JDND-C3 database
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             Logger.info("Connected " + connection.getMetaData().getDatabaseProductName());
+
+            try (Statement stmt = connection.createStatement()) {
+                // STEP 4: Use Statement to INSERT 2 records into the member table
+                // NOTE: The member table is created using Flyway by placing the migration file in src/main/resources/db/migration
+                stmt.execute("INSERT INTO jdnd_c3.`member` (name) VALUES ('Luan'), ('Mateuz')");
+                Logger.info("SQL executed");
+
+                ResultSet rs = connection.prepareStatement("SELECT * FROM member").executeQuery();
+
+                // STEP 5: Read ALL the rows from the member table and print them here
+                Logger.info("All rows from the member table");
+                Logger.info("id  name");
+                while (rs.next()) {
+                    String id = rs.getString(1);
+                    String name = rs.getString(2);
+                    Logger.info("{} {}", id, name);
+                }
+
+                // STEP 6: verify that all inserted rows have been printed
+                Logger.info("All rows printed");
+            }
         } catch (SQLException e) {
             Logger.error(e);
         }
-        // STEP 4: Use Statement to INSERT 2 records into the member table
-        // NOTE: The member table is created using Flyway by placing the migration file in src/main/resources/db/migration
 
-        // STEP 5: Read ALL the rows from the member table and print them here
-
-        // STEP 6: verify that all inserted rows have been printed
     }
 
 }
