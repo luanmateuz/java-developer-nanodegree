406dfa15b53d0554b3b1e60bb3a22c82f13e1978
luanmateuz <luanmateuzz@gmail.com>
2022-01-26 23:36:03 -0300

    exercise 01: flyway migrate and create table

--- part04/module01/exercises/l01_exercise01/src/main/java/com/udacity/lesson01/exercise01/Application.java
+++ part04/module01/exercises/l01_exercise01/src/main/java/com/udacity/lesson01/exercise01/Application.java
@@ -1,15 +1,26 @@
 package com.udacity.lesson01.exercise01;
 
+import org.flywaydb.core.Flyway;
+import org.tinylog.Logger;
 
 public class Application {
 
     public static void main(String[] args) {
         // STEP 1: Create the JDBC URL for JDND-C3 database
+        String url = "jdbc:mysql://localhost:3306/jdnd_c3?useSSL=false";
+        String username = "root";
+        String password = "root";
 
         // STEP 2: Setup and Run Flyway migration that creates the member table using its Java API
         // https://flywaydb.org/getstarted/firststeps/api#integrating-flyway
         // Note the above link talks about connecting to H2 database, for this exercise, MySQL is used. Adapt the code accordingly.
 
+        Flyway flyway = Flyway.configure()
+                .dataSource(url, username, password)
+                .load();
+
+        flyway.migrate();
+        Logger.info("Flyway Migrated");
 
         // STEP 3: Obtain a connection to the JDND-C3 database
 
--- part04/module01/exercises/l01_exercise01/src/main/resources/db/migration/V01__CreateTable.sql
+++ part04/module01/exercises/l01_exercise01/src/main/resources/db/migration/V01__CreateTable.sql
@@ -1,1 +1,9 @@
 -- write sql to create member table
+CREATE TABLE member (
+	id INT auto_increment,
+	name varchar(100) NULL,
+	CONSTRAINT NewTable_PK PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
