f8cbdfda8d9a50a91f990bea26d210ece1937dd5
luanmateuz <luanmateuzz@gmail.com>
2022-03-01 18:28:42 -0300

    project: db data backup

---
+++ part04/module01/projects/final/reviews/src/main/resources/db/data/reviews_final.sql
@@ -0,0 +1,13 @@
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `description`) VALUES
+(1, 'Thinkpad', 'notebook'),
+(2, 'Dell', 'PC'),
+(3, 'XBOX Series X', 'videogame microsoft'),
+(4, 'PS5', 'Sony'),
+(5, 'Service', 'ser')
+
+-- --------------------------------------------------------
